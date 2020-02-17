1ff19bfb4648a0580c44451e060e6984118bf7cd
rizMehdi <ccmeall@gmail.com>
2020-02-17 16:10:33 +0100

    test

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 /**
- * shows wink icon
+ * Provides access to basic micro:bit functionality.
  */
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
-namespace wink {
+namespace basic {
+    ...
+}
 
+//% block
+export function function showWink() {
     basic.showLeds(`
         . . . . .
         . # . # .
