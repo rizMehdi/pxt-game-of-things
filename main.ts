b1f221bcd4814b714ee610c4e7ec0097e667af39
rizMehdi <ccmeall@gmail.com>
2020-02-17 16:32:38 +0100

    testing multifunction export

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 namespace fun {
     /**
-    * Computes the famous Fibonacci number sequence!
-    */
+     * Provides access to basic micro:bit functionality.
+     */
+    //% color=100 weight=100 icon="\u1F340" block="Basic Blocks"
     //% block
     export function fib(value: number): number {
         return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
+    export function fib2(value: number): number 
+    {
+        value = fib(value - 1) + fib(value - 2);
+        return value
     }
 }
