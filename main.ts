d0fd1199d5b226b2e79deffd0fc0eedba499b380
rizMehdi <ccmeall@gmail.com>
2020-02-18 10:54:39 +0100

    working logbit, tweetbit functions

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ namespace skybit {
     //% username.defl="see sign-in card for info" pin.min=0000 pin.max=9999 pin.defl=0
     //% block
     export function tweetbitSign_in(username: string, pin: number): void {
+        radio.setTransmitSerialNumber(true)
         radio.sendValue(username, pin)
     }
     //% color=190 weight=99 icon="\u1F340" block="Basic Blocks"
@@ -23,19 +24,44 @@ namespace skybit {
     //% username.defl="see sign-in card for info" pin.min=0000 pin.max=9999 pin.defl=0
     //% block
     export function logbitSign_in(username: string, pin: number): void {
+        radio.setTransmitSerialNumber(true)
         radio.sendValue(username, pin)
     }
     //% color=190 weight=97 icon="\u1F340" block="Basic Blocks"
+    //% sensor_name.defl="sensor name"
     //% block
     export function logbitPost(sensor_name: string, value: number): void {
         radio.sendValue("&" + sensor_name, value)
     }
+
+
+
     //% color=190 weight=96 icon="\u1F340" block="Basic Blocks"
     //% block
     export function forecastbitRequest(forecastType: string): void {
-        radio.sendString(forecastType)
-        //and listen for reply? or listen seperately?
+        radio.sendString(forecastType)//and listen for reply? or listen seperately?
     }
+
+
+
+    //% color=190 weight=96 icon="\u1F340" block="Basic Blocks"
+    //% block
+    export function forecastbitRecieve(forecastType: string): number {
+        let temp = 9
+        // while (temp == -999) {
+        radio.sendString("get_" + forecastType)
+        radio.onReceivedValue(function (name: string, value: number) {
+            if (name == forecastType) {
+                temp = value;
+                basic.showNumber(value)
+            }
+        })
+        // }
+        return temp
+    }
+
+
+
     //% color=190 weight=94 icon="\u1F340" block="Basic Blocks"
     //% block
     export function triviabitRequest(): void {
@@ -56,7 +82,7 @@ namespace skybit {
 
 namespace p2pbit {
     /**
-     * Provides access to basic micro:bit functionality.
+     * Provides access to basic peer-to-peer functionality.
      */
     //% color=140 weight=100 icon="\u1F340" block="Basic Blocks"
     //% block
