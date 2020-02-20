4492528468d1dbfbf1e07e7b08222559b9c46e89
rizMehdi <ccmeall@gmail.com>
2020-02-20 16:26:09 +0100

    more langauge codes added in serialpi

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ namespace skybit {
     //% username.defl="see sign-in card for info" pin.min=0000 pin.max=9999 pin.defl=0
     //% block
     export function tweetbitSign_in(username: string, pin: number): void {
+        radio.setTransmitSerialNumber(true)
         radio.sendValue(username, pin)
     }
     //% color=190 weight=99 icon="\u1F340" block="Basic Blocks"
@@ -23,19 +24,49 @@ namespace skybit {
     //% username.defl="see sign-in card for info" pin.min=0000 pin.max=9999 pin.defl=0
     //% block
     export function logbitSign_in(username: string, pin: number): void {
+        radio.setTransmitSerialNumber(true)
         radio.sendValue(username, pin)
     }
     //% color=190 weight=97 icon="\u1F340" block="Basic Blocks"
+    //% sensor_name.defl="sensor name"
     //% block
     export function logbitPost(sensor_name: string, value: number): void {
         radio.sendValue("&" + sensor_name, value)
     }
+
+
+
     //% color=190 weight=96 icon="\u1F340" block="Basic Blocks"
+    //% forecastType.defl="get_xyz"
     //% block
     export function forecastbitRequest(forecastType: string): void {
-        radio.sendString(forecastType)
-        //and listen for reply? or listen seperately?
+        radio.sendString(forecastType)//and listen for reply? or listen seperately?
     }
+
+    //% color=190 weight=96 icon="\u1F340" block="Basic Blocks"
+    //% forecastType.defl="xyz"
+    //% block
+    export function forecastbitGet(forecastType: string): void {
+        radio.sendString("get_" + forecastType)
+    }
+
+
+    //% color=190 weight=96 icon="\u1F340" block="Basic Blocks"
+    //% block
+    export function forecastbitRecieve(forecastType: string): number {
+        let temp = 9
+        // while (temp == -999) {
+        radio.sendString("get_" + forecastType)
+        radio.onReceivedValue(function (name: string, value: number) {
+            basic.showNumber(value)
+            temp = value;
+        })
+        // }
+        return temp
+    }
+
+
+
     //% color=190 weight=94 icon="\u1F340" block="Basic Blocks"
     //% block
     export function triviabitRequest(): void {
@@ -56,7 +87,7 @@ namespace skybit {
 
 namespace p2pbit {
     /**
-     * Provides access to basic micro:bit functionality.
+     * Provides access to basic peer-to-peer functionality.
      */
     //% color=140 weight=100 icon="\u1F340" block="Basic Blocks"
     //% block
@@ -75,3 +106,33 @@ namespace p2pbit {
     }
 }
 
+
+namespace serialpi {
+    /**
+     * Provides access to basic peer-to-peer functionality.
+     */
+    //% color=140 weight=100 icon="\u1F340" block="Basic Blocks"
+    //% langcode.defl="it/de/en/fr/ur/el/es" text2speak.defl="put your text here"
+    //% block
+    export function textToSpeech_serial_(langcode: string, text2speak: string): void {
+        serial.writeLine(langcode + text2speak)
+    }
+    //% color=140 weight=100 icon="\u1F340" block="Basic Blocks"
+    //% langcode.defl="it/de/en/fr/ur/el/es" smalltext.defl="<16CharText>"
+    //% block
+    export function textToSpeech_ota_(langcode: string, smalltext: string): void {
+        let shortcode='e'
+        switch (langcode) {
+            case "it": shortcode = 'i'; break;
+            case "de": shortcode = 'd'; break;
+            case "en": shortcode = 'e'; break;
+            case "ur": shortcode = 'u'; break;
+            case "fr": shortcode = 'f'; break;
+            case "el": shortcode = 'g'; break;
+            case "es": shortcode = 's'; break;
+            case "ar": shortcode = 'a'; break; 
+            default: shortcode = 'x'; break;
+        }
+        radio.sendString(shortcode + "$" + smalltext)
+    }
+}
